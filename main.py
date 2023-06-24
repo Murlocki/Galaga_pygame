6f63453edb53e5f668a2d8c042adc5dbefd18dce
Murlocki <kirillkornilov03@gmail.com>
2023-06-24 22:13:55 +0300

    Main version 0.1.0 Movement and bullet are added

--- main.py
+++ main.py
@@ -1,29 +1,92 @@
 import sys
 import pygame
 from settings import Settings
-from ship import Ship
+from Ship import Ship
+from bullet import Bullet
+from pygame.sprite import Group
+
+# Пишем функцию для проверки ивентов
+def check_events(ship,settings, screen,bullets):
+    for event in pygame.event.get():
+        match event.type:
+            case pygame.QUIT:
+                sys.exit()
+            case pygame.KEYDOWN:
+                check_keydown_events(event,ship, settings, screen, bullets)
+            case pygame.KEYUP:
+                check_keyup_events(event,ship)
+
+
+def check_keydown_events(event, ship, settings, screen, bullets):
+    match event.key:
+        case pygame.K_RIGHT:
+            ship.moving_right = True
+        case pygame.K_LEFT:
+            ship.moving_left = True
+        case pygame.K_UP:
+            ship.moving_up = True
+        case pygame.K_DOWN:
+            ship.moving_down = True
+        case pygame.K_SPACE:
+            if len(bullets) < settings.bullets_allowed:
+                new_bullet = Bullet(settings, screen, ship)
+                bullets.add(new_bullet)
+
+def check_keyup_events(event, ship):
+    match event.key:
+        case pygame.K_RIGHT:
+            ship.moving_right = False
+        case pygame.K_DOWN:
+            ship.moving_down = False
+        case pygame.K_UP:
+            ship.moving_up = False
+        case pygame.K_LEFT:
+            ship.moving_left = False
+
+# Обновление пуль
+def update_bullets(bullets):
+    # Рисуем пули
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+# Функция обновления экрана
+def update_screen(settings,screen,ship,bullets):
+    # Заполняем задний фон
+    screen.fill(settings.background_color)
+
+    # Рисуем корабль
+    ship.blitme()
+
+    # Рисуем пули
+    update_bullets(bullets)
+    # Обновляем экран
+    pygame.display.flip()
 
 def run_game():
     # Инициализация pygame
     pygame.init()
 
     # Получаем разрешение экрана
-    settings=Settings()
+    settings = Settings()
     screen = pygame.display.set_mode((settings.width, settings.heigth))
 
     # Заголовок окна
     pygame.display.set_caption('Galaga')
 
     # Создаем корабль игрока
-    ship = Ship()
-    # Обработка событий от пользователя
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
+    ship = Ship(screen,settings)
 
-        #Заполняем и перерисовываем экран
-        screen.fill(settings.background_color)
-        ship.blitme()
-        pygame.display.flip()
+    # Создаем группу пуль
+    bullets = Group()
+    # Основной цикл игры
+    while True:
+        check_events(ship, settings, screen, bullets)
+        ship.update()
+        bullets.update()
+        update_screen(settings, screen, ship, bullets)
+
+
+
 run_game()
--- settings.py
+++ settings.py
@@ -7,3 +7,13 @@ class Settings():
     def __init__(self):
         self.background_color = (255,255,255)
         self.width, self.heigth = Resolution_return()
+
+        # Параметры движения
+        self.speed_factor = 1.5
+
+        # Параметры пули
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_heigth = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 5
